14476ea1d74fd217c08690e9bc97be7ae3b421e8
Bradford Toney <bradford.toney@gmail.com>
2013-06-05 11:10:59 -0700

    XMPPFramework

---
+++ Core/XMPPFramework.h
@@ -0,0 +1,20 @@
+
+#import "XMPP.h"
+
+// List the modules you're using here.
+
+#import "XMPPReconnect.h"
+
+#import "XMPPLogging.h"
+
+#import "XMPPAutoPing.h"
+
+#import "NSXMLElement+XMPP.h"
+#import "NSXMLElement+MGPubSub.h"
+#import "XMPPIQ+MGPubSubAdditions.h"
+#import "XMPPMessage+MGPubSubAdditions.h"
+#import "XMPPStream+MGXMPPStreamAdditions.h"
+
+
+//static const int xmppLogLevel = XMPP_LOG_LEVEL_VERBOSE | XMPP_LOG_FLAG_TRACE;
+
