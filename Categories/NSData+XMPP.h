ce3532bcff01d9a365a684fd34f52c2b79f44d52
Bradford Toney <bradford.toney@gmail.com>
2013-06-15 15:11:59 -0700

    Adding categories

--- Categories/NSData+XMPP.h
+++ Categories/NSData+XMPP.h
@@ -1,5 +1,16 @@
 #import <Foundation/Foundation.h>
 
+void *NBase64Decode(
+                      const char *inputBuffer,
+                      size_t length,
+                      size_t *outputLength);
+
+char *NBase64Encode(
+                      const void *inputBuffer,
+                      size_t length,
+                      bool separateLines,
+                      size_t *outputLength);
+
 @interface NSData (XMPP)
 
 - (NSData *)md5Digest;
@@ -10,5 +21,8 @@
 
 - (NSString *)base64Encoded;
 - (NSData *)base64Decoded;
+
++ (NSData *)dataFromBase64String:(NSString *)aString;
+- (NSString *)base64EncodedString;
 
 @end
